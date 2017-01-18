fa3a14368b31f366df7c44617cf0660379c46bff
Xavier Huberdeau <xavier.huberdeau@gmail.com>
2017-01-18 10:21:25 +0100

    Fix import mapping + add script to get number of call around landsale

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,55 +7,80 @@ var esClient = new elasticsearch.Client({
 	log: 'error'
 });
 
-// first create mapping to specify geo point mainly
-var mapping = {
-	call: {
-		properties: {
-			location: { "type": "geo_point" },
-			desc: { "type": "string" },
-			zip: { "type": "string" },
-			type: { "type": "string" },
-			category: { "type": "string" },
-			timeStamp: { "type": "string" },
-			address: { "type": "string" },
-			city: { "type": "string" }
-		}
-	}
-};
-esClient.indices.putMapping({ index: "calls", type: "call", body: mapping });
 
-// array of 911 calls to push
-var calls = [];
-// parse csv
-fs.createReadStream('../911.csv')
-	.pipe(csv())
-	.on('data', data => {
-		// where the call will go
-		calls.push(
-			{ "index": { "_index": "calls", "_type": "call" } }
-		);
-		// add new call object
-		calls.push(
-			{
-				location: {
-					lat: data.lat,
-					lon: data.lng
-				},
-				desc: data.desc,
-				zip: data.zip,
-				type: data.title.substr(0, data.title.indexOf(":")).trim(),
-				category: data.title.substr(data.title.indexOf(":") + 1).trim(),
-				timeStamp: data.timeStamp,
-				address: data.addr,
-				city: data.twp
-			}
-		);
-	})
-	.on('end', () => {
-		// bulk insert the calls we've prepared
-		esClient.bulk({ body: calls })
-			.then(response => {
-				console.log(response);
-			}
-			)
+function createCallIndex() {
+	console.log("Creating index ...");
+	return esClient.indices.create({
+		index: 'calls'
 	});
+}
+
+function createMapping() {
+	console.log("Creating mapping ...");
+	var mapping = {
+		properties: {
+			"location": { "type": "geo_point" },
+			"desc": { "type": "string" },
+			"zip": { "type": "string" },
+			"type": { "type": "string" },
+			"category": { "type": "string" },
+			"timeStamp": { "type": "string" },
+			"address": { "type": "string" },
+			"city": { "type": "string" }
+		}
+	};
+	return esClient.indices.putMapping({ index: "calls", type: "call", body: mapping });
+}
+
+createCallIndex().then(function (response) {
+	console.log(response);
+	createMapping().then(function (response) {
+		console.log(response);
+		insertCalls();
+	}, function(error){
+		console.log("Error ! :"+error);
+	});
+}, function(error){
+	console.log("Error ! :"+error);
+});
+
+function insertCalls() {
+	console.log("Inserting calls ...");
+	// array of 911 calls to push
+	var calls = [];
+	// parse csv
+	fs.createReadStream('../911.csv')
+		.pipe(csv())
+		.on('data', data => {
+			// where the call will go
+			calls.push(
+				{ "index": { "_index": "calls", "_type": "call" } }
+			);
+			// add new call object
+			calls.push(
+				{
+					"location": {
+						lat: data.lat,
+						lon: data.lng
+					},
+					"desc": data.desc,
+					"zip": data.zip,
+					"type": data.title.substr(0, data.title.indexOf(":")).trim(),
+					"category": data.title.substr(data.title.indexOf(":") + 1).trim(),
+					"timeStamp": data.timeStamp,
+					"address": data.addr,
+					"city": data.twp
+				}
+			);
+		})
+		.on('end', () => {
+			// bulk insert the calls we've prepared
+			esClient.bulk({ index: "calls", type: "call", body: calls })
+				.then(response => {
+					console.log(response);
+				}, function(error){
+					console.log("Error ! :"+error);
+				})
+		});
+}
+
---
+++ elasticsearch/number_calls_around_landsale.js
@@ -0,0 +1,36 @@
+var elasticsearch = require('elasticsearch');
+var esClient = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'error'
+});
+
+var lat = 40.241493;
+var lon =  -75.283783;
+var distance = "500m";
+console.log("Searching calls around (lat:"+lat+" , lon:"+lon+") for a distance of "+distance);
+esClient.search({
+    index: 'calls',
+    type: 'call',
+    body: {
+        "query": {
+            "bool": {
+                "must": {
+                    match_all: {}
+                },
+                "filter": {
+                    "geo_distance": {
+                        "distance": distance,
+                        "location": {
+                            "lat": 40.241493,
+                            "lon": -75.283783
+                        }
+                    }
+                }
+            }
+        }
+    }
+}).then(function (response) {
+    console.log("Found : "+response.hits.total+ " calls");
+}, function (error) {
+    console.log("Error ! :"+error);
+});
